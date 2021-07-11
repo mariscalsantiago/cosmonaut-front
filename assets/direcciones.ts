351d86342cd3229e88107b33a9401a28f882a8ed
santiago Antonio Mariscal Velasquez <samvprogrammer@Air-de-santiago.lan>
2021-07-10 19:44:00 -0500

    naye version

--- assets/direcciones.ts
+++ assets/direcciones.ts
@@ -2,11 +2,17 @@ import {environment} from './../environments/environment';
 
 const ip:string = environment.rutaEmpresa;
 const ip3:string = environment.rutaCatalogos;
-const ipCalculos:string = environment.rutaCalculos;
+const ipCalculos:string = environment.rutaNomina;
+const ipNomina:string = environment.rutaNomina;
+const ipAdmin: string = environment.rutaAdmin;
+const ipReportes:string = environment.rutaReportes;
+const ipSocket:string = environment.rutaSocket;
 
 export const direcciones = {
 
+    
     usuarios:`${ip}/persona`,
+    empleado:`${ip}/empleado`,
     centroCostosCliente: `${ip}/centroCostosCliente`,
     area: `${ip}/area`,
     politica: `${ip}/Politica`,
@@ -20,9 +26,40 @@ export const direcciones = {
     contratoColaborador:`${ip}/contratoColaborador`,
     nclPolitica:`${ip}/Politica`,
     sedes:`${ip}/sede`,
-	reportes:`${ip}/reportes`,
+	reportes:`${ipReportes}`,
     registroPatronal:`${ip}/registroPatronal`,
     conceptos:`${ip}/percepcionDeduccion`,
     incidencias:`${ip}/incidencias`,
-    orquestador:`${ipCalculos}/orquestador`
+    documentos:`${ip}/documentos`,
+    kardex:`${ip}/kardex`,
+    tablasValores: `${ipAdmin}/TablasValores`,
+    tablasValoresCAT: `${ip3}/TablasValores`,
+    adminCatalogo: `${ipAdmin}/adminCatalogo`,
+    adminCatalogoDisp: `${ipAdmin}/admin-catalogo`,
+    roles: `${ipAdmin}/rol`,
+    modulos: `${ipAdmin}/modulos`,
+    permisos: `${ipAdmin}/permisos`,
+    chat: `${ip}/chat`,
+    socket: `${ipSocket}`,
+    empresa: `${ip}`,
+    usuariosAuth:`${ipAdmin}/usuarios`,
+    versiones:`${ipAdmin}/version`,
+    ptu:`${ip}/ptu`,
+    oauth:`${ipAdmin}/oauth`,
+    nominaOrdinaria:`${ipCalculos}/nomina-ordinaria`,
+    nominaExtraordinaria:`${ipCalculos}/nomina-extraordinaria`,
+    nominaLiquidacion:`${ipCalculos}/nomina-liquidacion`,
+    nominaPtu:`${ipCalculos}/nomina-ptu`,
+    variabilidad:`${ipCalculos}/variabilidad`,
+    orquestador:`${ipCalculos}/orquestador`,
+    calculo:`${ipCalculos}/calculo`,
+    imss: `${ip}/imss`,
+    bitacoramovimientos: `${ipAdmin}/bitacora-movimientos`,
+    dispersion:`${ipCalculos}/dispersion`,
+    tipomensaje:`${ip3}/tipo-mensaje`,
+    administrarMensajeChat:`${ipAdmin}/administrar-mensaje-chat`,
+    nominasHistoricas: `${ipCalculos}/nominas-historicas`,
+    timbrado:`${ipCalculos}/timbrado`,
+    timbradoAdmin:`${ipNomina}/timbrado`
+
 };
